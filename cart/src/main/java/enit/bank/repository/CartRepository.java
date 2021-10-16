61ac5ff727a664be2a49dce4ee1034515902cab6
meher-guesmi <76886909+meher-guesmi@users.noreply.github.com>
2021-10-16 18:57:40 +0100

    CartRepo

--- cart/src/main/java/enit/bank/repository/CartRepository.java
+++ cart/src/main/java/enit/bank/repository/CartRepository.java
@@ -1,5 +1,33 @@
 package enit.bank.repository;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import enit.bank.domain.Cart;
+import enit.bank.service.CartService;
+
+@ApplicationScoped
 public class CartRepository {
+
+    @Inject
+    EntityManager em;
+
+    public Cart insert(CartService cartService) {
+        return null;
+    }
+
+    public Cart findById(Integer id) {
+        Cart c=null;
+        return c;
+    }
+
+    public Cart update(Cart cart) {
+        return null;
+    }
+
+    public String deleteById(Integer id) {
+        return "Cart deleted";
+    }
     
 }
