d3a600f6f634dc7092377d1ced44ae5840d25ddb
Zeineb <zeinebsmidi@gmail.com>
2021-10-17 21:36:41 +0100

    first commit api

--- orders/src/main/java/enit/bank/api/OrdersResource.java
+++ orders/src/main/java/enit/bank/api/OrdersResource.java
@@ -1,21 +1,36 @@
 package enit.bank.api;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import enit.bank.domain.Orders;
+import enit.bank.service.OrdersService;
 
 
-@Path("/api/persons")
+
+@Path("/Orders")
 public class OrdersResource {
 
    
+    private OrdersService ordersService;
+    public OrdersResource(OrdersService service) {
+        this.ordersService = service;
+    }
+
+    @POST
+    @Path("/create")
+    public Orders createOrder(Orders order) {
+        return ordersService.create(order);
+    }
+
     @GET
-    @Path("/hello")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Path("/{id}")
+    public Orders findOrderById(@PathParam("id") Long id) {
+        return ordersService.findById(id);
     }
 
 }
