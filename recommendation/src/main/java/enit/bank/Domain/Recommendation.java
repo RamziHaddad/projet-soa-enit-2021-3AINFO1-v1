e3bc8519a0b30dbf972cac98b531296303f62a29
nahedBA <91974170+nahedBA@users.noreply.github.com>
2021-10-17 17:54:00 +0100

    firstAlgo

--- recommendation/src/main/java/enit/bank/Domain/Recommendation.java
+++ recommendation/src/main/java/enit/bank/Domain/Recommendation.java
@@ -12,4 +12,26 @@ public class Recommendation {
         this.nbrOccurrences = 1;
         ;
     }
+
+    public void setNbrOccurrences(int nbrOccurrences) {
+        this.nbrOccurrences += nbrOccurrences;
+    }
+    public int getNbrOccurrences() {
+        return nbrOccurrences;
+    }
+    public boolean compareTo(Recommendation r) {
+
+        return (this.productId1 == r.productId1 && this.productId2==r.productId2) || (this.productId1 == r.productId2 && this.productId2==r.productId1) ;
+     }
+    public String getProductId1() {
+        return productId1;
+    }
+    public String getProductId2() {
+        return productId2;
+    }
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return this.productId1+","+this.productId2+","+this.nbrOccurrences;
+    }
 }
--- recommendation/src/main/java/enit/bank/TestResource.java
+++ recommendation/src/main/java/enit/bank/TestResource.java
@@ -5,7 +5,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-
+import enit.bank.Domain.Recommendation;
+import enit.bank.Algo.Algorithme;
+import java.util.*;
 
 @Path("/api/persons")
 public class TestResource {
@@ -15,7 +17,20 @@ public class TestResource {
     @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        List<Recommendation> recommendations=new ArrayList<>();
+        recommendations.add(new Recommendation("12","1"));
+        recommendations.add(new Recommendation("15", "10"));
+        Algorithme algo=new Algorithme();
+        List<String> listId=new ArrayList<>();
+        listId.add("1");
+        listId.add("12");
+        listId.add("2");
+        var table=algo.algo(listId, recommendations);
+        String s="";
+        for(int i=0;i<table.size();i++){
+            s+=table.get(i).toString()+"\n";
+        }
+        return s;
     }
 
 }
