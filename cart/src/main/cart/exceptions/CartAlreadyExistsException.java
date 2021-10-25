696aade4ff489e9ecbccb931394da6f145ff1bfa
Mayssa Mathlouthi <mayssa.mathlouthi@etudiant-enit.utm.tn>
2021-10-25 09:04:28 +0100

    fixing service and execptions

--- cart/src/main/cart/exceptions/CartAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package enit.bank.exceptions;
-
-public class CartAlreadyExistsException  extends Exception{
-    public CartAlreadyExistsException(String msg) {
-        super(msg);
-    }
-}
--- cart/src/main/cart/service/CartService.java
+++ cart/src/main/cart/service/CartService.java
@@ -7,33 +7,5 @@ import enit.bank.repository.CartRepository;
 import enit.bank.exceptions.*;
 
 public class CartService {
-
-    @Inject
-    CartRepository cartRepository;
-    public CartService(CartRepository cartRepo){
-        this.cartRepository = cartRepo;
-     } 
-
-     public Cart create(Cart p) throws CartAlreadyExistsException{
-        return cartRepository.insert(p);
-    }
-
-    public Object addToCart(Cart cart) {
-        return cartRepository.insert(cart);
-    }
-
-    public Cart findById(Integer id){
-        return cartRepository.findById(id);
-    }
-
-    public Cart update(Cart cart){
-        return cartRepository.update(cart);
-    }
-
-    public void deleteCart(Integer id) {
-        cartRepository.deleteById(id);
-    }
-
-
     
 }
