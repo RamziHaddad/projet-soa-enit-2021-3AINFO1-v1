08387214083b91bb863c87baa2a319096ead32ef
Nour-Rekik <nour.rekik@etudiant-enit.utm.tn>
2021-10-25 09:32:00 +0100

    map_DTOs

--- payment/src/main/java/enit/bank/service/dto/PaymentFoaAddDTO.java
+++ payment/src/main/java/enit/bank/service/dto/PaymentFoaAddDTO.java
@@ -2,8 +2,7 @@ package enit.bank.service.dto;
 
 import java.time.Instant;
 
-public class PaymentDTO {
-    private Long id;
+public class PaymentFoaAddDTO {
     private String paymentMode;
 
     private String cartName;
@@ -11,13 +10,13 @@ public class PaymentDTO {
     private Long cartNumber;
     private Long secretCode;
     private Instant ExpirationDate;
-
-    public PaymentDTO() {
+    
+    public PaymentFoaAddDTO() {
     }
 
-    public PaymentDTO(Long id, String paymentMode, String cartName, Long cartNumber, Long secretCode,
+    
+    public PaymentFoaAddDTO(String paymentMode, String cartName, Long cartNumber, Long secretCode,
             Instant expirationDate) {
-        this.id = id;
         this.paymentMode = paymentMode;
         this.cartName = cartName;
         this.cartNumber = cartNumber;
@@ -25,57 +24,36 @@ public class PaymentDTO {
         ExpirationDate = expirationDate;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getPaymentMode() {
         return paymentMode;
     }
-
     public void setPaymentMode(String paymentMode) {
         this.paymentMode = paymentMode;
     }
-
     public String getCartName() {
         return cartName;
     }
-
     public void setCartName(String cartName) {
         this.cartName = cartName;
     }
-
     public Long getCartNumber() {
         return cartNumber;
     }
-
     public void setCartNumber(Long cartNumber) {
         this.cartNumber = cartNumber;
     }
-
     public Long getSecretCode() {
         return secretCode;
     }
-
     public void setSecretCode(Long secretCode) {
         this.secretCode = secretCode;
     }
-
     public Instant getExpirationDate() {
         return ExpirationDate;
     }
-
     public void setExpirationDate(Instant expirationDate) {
         ExpirationDate = expirationDate;
     }
-    
-
-
-
-    
     
 }
