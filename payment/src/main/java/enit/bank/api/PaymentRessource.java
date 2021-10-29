cebaf5404bca5f25844840422bb240524e729386
Nour-Rekik <nour.rekik@etudiant-enit.utm.tn>
2021-10-29 20:45:40 +0100

    withdraw_money

--- payment/src/main/java/enit/bank/api/PaymentRessource.java
+++ payment/src/main/java/enit/bank/api/PaymentRessource.java
@@ -1,8 +1,5 @@
 package enit.bank.api;
 
-
-import java.math.BigDecimal;
-
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.GET;
@@ -21,12 +18,13 @@ import enit.bank.exceptions.EntityAlreadyExistsException;
 import enit.bank.exceptions.EntityNotFoundException;
 import enit.bank.service.BankService;
 import enit.bank.service.PaymentService;
+import enit.bank.service.dto.BankAccountDTO;
 import enit.bank.service.dto.PaymentForAddDTO;
 
 @Path("/api/payments")
 public class PaymentRessource {
     @RestClient
-    BankService bankService; 
+    BankService remoteBankService; 
 
     @Inject
     PaymentService paymentService;
@@ -53,8 +51,9 @@ public class PaymentRessource {
     @POST
     @Transactional
     public Response createPayment(@RequestBody PaymentForAddDTO paymentForAddDTO) throws EntityAlreadyExistsException {
+       BankAccountDTO bankAccountDTO= this.remoteBankService.getBankAccountByAccountNumber(paymentForAddDTO.getFromAccountNumber());
+       this.remoteBankService.withdrawMoneyFromAccount(paymentForAddDTO.getAmount(), bankAccountDTO.getId());
        Payment paymentAdded= this.paymentService.addPayment(paymentForAddDTO);
-       BigDecimal remainingAmount = this.bankService.withdrawMoneyFromAccount(money, id)
        return Response.ok(paymentAdded).build();
     }
 
